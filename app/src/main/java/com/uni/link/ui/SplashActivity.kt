1c337161dc61a44afc19ccf935e0870bb2605e72
Bugorf <Bugorf@Bugorf.com>
2023-12-13 15:32:49 +0100

    splash

--- app/src/main/java/com/uni/link/ui/SplashActivity.kt
+++ app/src/main/java/com/uni/link/ui/SplashActivity.kt
@@ -1,51 +1,24 @@
+package eu.mobeepass.nfcniceticket
+
+import android.R
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
-import kotlinx.androix.synthetic.main.activity_splash.*
-import com.uni.link.R
+import com.uni.link.MainActivity
+import java.util.*
 
-class SplashActivity : AppCompatActivity() {
-
-    private val logos = listOf(
-        R.drawable.book1,
-        R.drawable.book2,
-        R.drawable.football,
-        R.drawable.basketball,
-        R.drawable.tennisball
-    )
-
-    private var currentLogoIndex = 0
-
+class splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
-
-        // Load the first logo and apply a transition effect
-        Glide.with(this)
-            .load(logos[currentLogoIndex])
-            .transition(DrawableTransitionOptions.withCrossFade())
-            .into(splash_image)
-
-        // Start a handler to update the logo every second
-        Handler().postDelayed({
-            updateLogo()
-        }, 1000)
-    }
-
-    private fun updateLogo() {
-        currentLogoIndex = (currentLogoIndex + 1) % logos.size
-        Glide.with(this)
-            .load(logos[currentLogoIndex])
-            .transition(DrawableTransitionOptions.withCrossFade())
-            .into(splash_image)
-
-        // Update the logo every second
-        Handler().postDelayed({
-            updateLogo()
-        }, 1000)
+        setContentView(R.layout.list_content)
+        val mTimer = Timer()
+        val mTimeTask: TimerTask = object : TimerTask() {
+            override fun run() {
+                val intent = Intent(this@splash, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+        mTimer.schedule(mTimeTask, 1500)
     }
 }
