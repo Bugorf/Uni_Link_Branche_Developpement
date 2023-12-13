4c4b7bf5ba128757565c8832c0a66d6ab9fd1337
Bugorf <Bugorf@Bugorf.com>
2023-12-13 15:02:35 +0100

    valid

--- app/src/main/java/com/uni/link/pages/message/MessageFragment.kt
+++ app/src/main/java/com/uni/link/pages/message/MessageFragment.kt
@@ -13,7 +13,10 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.uni.link.databinding.FragmentMessageBinding
@@ -21,9 +24,7 @@ import com.uni.link.databinding.FragmentMessageBinding
 class MessageFragment : Fragment() {
 
     private var _binding: FragmentMessageBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    private lateinit var adapter: MsgAdapter
     private val binding get() = _binding!!
 
     private lateinit var auth: FirebaseAuth
@@ -38,29 +39,39 @@ class MessageFragment : Fragment() {
 
         val btn: Button = binding.button2
         val et: EditText = binding.editTextText
+        val lv: ListView = binding.lv
 
+        val database = Firebase.database
+        val conversationsRef = database.getReference("conversations")
+        val t: TextView = binding.textView6
+        val idList = mutableListOf<String>()
+
+        adapter = MsgAdapter(requireContext(), idList)
+        lv.adapter = adapter
 
         btn.setOnClickListener {
-            // 获取 Firebase 实时数据库的引用
-            val database = FirebaseDatabase.getInstance()
-            val conversationsRef = database.getReference("conversations")
 
-// 创建一个唯一的聊天会话标识符
-            val conversationId = conversationsRef.push().key
-
-// 在 conversations 节点下创建一个聊天会话
-            val conversationData = HashMap<String, Any>()
-            conversationData["messages"] = HashMap<String, Any>()
-            conversationData["participants"] = HashMap<String, Boolean>()
-
-            conversationsRef.child(conversationId!!).setValue(conversationData)
-
-
-
-
-
+            val value = et.text.toString()
+            conversationsRef.setValue(value)
 
         }
+
+        conversationsRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+
+                val conId = dataSnapshot.value.toString()
+                t.text = conId
+
+
+
+            }
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
+
+
 
 
         return root
